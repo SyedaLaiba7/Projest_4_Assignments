867f662254b5e6986896414444046e8977df1fab
syedalaiba <syedalaiba5645776@gmail.com>
2025-03-24 02:17:25 +0500

    project

---
+++ TASK-1/Assignments_00_05/03_if_statements/01_print_events/main.py
@@ -0,0 +1,8 @@
+def main():
+    """Prints the first 20 even numbers."""
+    for i in range(20):
+        print(i * 2, end=" ")
+    print()  # New line after printing all numbers
+
+if __name__ == "__main__":
+    main()
